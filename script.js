3d5cac4ebd85e511fe44a1381ed86746f4e517e0
sierra25 <88254705+sierra25@users.noreply.github.com>
2022-04-25 09:48:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+let title=document.querySelector("#text");
+let notesElement=document.querySelector('.notes');
+
+let description=document.querySelector('#description');
+let submit=document.querySelector(".submit");
+
+let notes=JSON.parse(localStorage.getItem("notes"));
+if(notes){
+    notes.forEach(element => {
+        addNotes(element)
+    });
+}
+/*When user clicks add button it will add the note to the screen and local storage*/
+submit.addEventListener("click", (e)=>{
+    e.preventDefault();
+    addNotes()
+})
+function addNotes(obj) {
+    let card=document.createElement("div");
+    card.classList.add("card");
+    let titleval=title.value;
+    let descVal=description.value;
+    if(obj){
+        titleval=obj.title;
+        descVal=obj.description;
+    }
+    /*Writes the users note into the note card element*/
+    if(titleval){
+        card.innerHTML=`<h3>${titleval}</h3>
+                                    <p class="ptag">${descVal}</p>
+                             <button class="delete">Delete</button>`;
+        notesElement.appendChild(card);
+        updateLs()
+    }
+    let deleteNote=card.querySelector(".delete");
+    deleteNote.addEventListener('click', ()=>{
+        card.remove();
+        updateLs();
+    })
+}
+function updateLs() {
+    let card=document.querySelectorAll(".card");
+    let arr=[];
+    card.forEach(element => {
+        arr.push({
+            title:element.children[0].innerText,
+            description:element.children[1].innerText
+        })
+    });
+    localStorage.setItem("notes", JSON.stringify(arr));
+}
